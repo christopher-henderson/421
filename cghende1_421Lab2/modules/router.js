fcb1a0d73c94fbbf6e6aa3b211bad3cae261603c
christopher-henderson <chris@chenderson.org>
2016-09-23 14:59:12 -0700

    regexp based router

--- cghende1_421Lab2/modules/router.js
+++ cghende1_421Lab2/modules/router.js
@@ -3,34 +3,49 @@ var qstring = require('querystring');
 
 function Router() {
 
-  this.handlers = {};
+  this.patterns = [];
 
-  this.addContext = function(pathname, handler) {
-    this.handlers[pathname] = handler;
-  }
+  this.addContext = function(pattern, handler) {
+    this.patterns.push(new Pattern(pattern, handler || new Handler()));
+  };
 
   this.route = function(req, res) {
+    // Not particularly efficient, but I want the regular expression matching
+    // over constant time routing in this assignment.
     var urlObj = url.parse(req.url, true, false);
     var pathname = urlObj.pathname;
-    console.log(pathname);
-    handler = this.handlers[pathname];
-    if (handler === undefined) {
-      console.log('404');
-      res.writeHead(404);
-      res.end('lol no');
-      return
+    for (var index=0; index < this.patterns.length; index++) {
+      var handler = this.patterns[index].handlerOrNull(pathname);
+      if (handler !== null) {
+        return handler.handle(req, res);
+      }
     }
-    handler.handle(req, res);
-  }
+    return NotFound.handle(req, res);
+  };
 }
 
-function Handler(req, res) {
+function Pattern(pattern, handler) {
+  this.pattern = new RegExp(pattern);
+  this.handler = handler;
+  this.handlerOrNull = function(path) {
+    return (this.pattern.exec(path) !== null) ? this.handler : null;
+  };
+}
+
+function Handler() {
   this.handle = function(req, res) {
     console.log("500");
     res.writeHead(500);
     res.end('WHOOPS!');
-  }
+  };
 }
+
+var NotFound = Object.create(Handler);
+NotFound.handle = function(req, res) {
+  console.log('404');
+  res.writeHead(404);
+  res.end('lol no');
+};
 
 exports.Handler = Handler;
 exports.Router = Router;
--- cghende1_421Lab2/server.js
+++ cghende1_421Lab2/server.js
@@ -1,5 +1,5 @@
-var File = require("./modules/file.js")
-var r = require("./modules/router.js")
+var File = require("./modules/file.js");
+var r = require("./modules/router.js");
 var http = require('http');
 
 
@@ -8,16 +8,16 @@ Awesome.handle = function(req, res) {
   console.log('200');
   res.writeHead(200);
   res.end('yay!');
-}
+};
 
 
 
 router = new r.Router();
 
-router.addContext("/lol", Awesome)
-router.addContext("/asdas", new r.Handler())
-
+router.addContext("^/lol/?$", Awesome);
+router.addContext("/asdas", new r.Handler());
+router.addContext('/uhoh');
 
 http.createServer(function (request, response) {
-  router.route(request, response)
+  router.route(request, response);
 }).listen(1337);
