91c565ff9e8363ac557e5b13d1b4a0dc36c71d99
christopher-henderson <chris@chenderson.org>
2016-09-26 09:48:25 -0700

    semicolon

--- cghende1_421Lab2/routes/routes.js
+++ cghende1_421Lab2/routes/routes.js
@@ -1,7 +1,7 @@
 var LoginLogout = require("./LoginLogout.js");
 var Static = require("./Static.js");
 var Landing = require("./Landing.js");
-var Login = require("./LoginPage.js")
+var Login = require("./LoginPage.js");
 
 module.exports = [
   ['^/media/.*$', Static],
