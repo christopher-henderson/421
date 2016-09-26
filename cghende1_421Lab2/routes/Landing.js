58f3ccaabd20457d1827059095620704cd3512d1
christopher-henderson <chris@chenderson.org>
2016-09-26 08:29:47 -0700

    landing page

--- cghende1_421Lab2/routes/Landing.js
+++ cghende1_421Lab2/routes/Landing.js
@@ -3,13 +3,23 @@ var path = require("path");
 var url = require('url');
 
 var hf = require("../modules/HeaderFooter.js");
+var story = require("../modules/story.js");
 var r = require("../router/router.js");
 var Handler = r.Handler;
 
 var Landing = Object.create(Handler);
 Landing.handle = function(req, res) {
-  res.writeHead(200);
-  res.end(hf.buildPage(req, "<br>LOL HI!<br>"));
+  story.getStories(function(stories) {
+    var page = "";
+    for (var index in stories) {
+      var story = stories[index];
+      if (req.loggedIn || story.public) {
+        page += "<h2>" + story.title + "</h2><br>";
+      }
+    }
+    res.writeHead(200);
+    res.end(hf.buildPage(req, page));
+  });
 };
 
 module.exports = Landing;
