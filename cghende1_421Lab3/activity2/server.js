37e20043f62af79a2da2a41d191cb638a0d27d5e
christopher-henderson <chris@chenderson.org>
2016-10-11 11:38:24 -0700

    simple part of activity 2

--- cghende1_421Lab3/activity2/server.js
+++ cghende1_421Lab3/activity2/server.js
@@ -7,15 +7,16 @@ var mimeTypes = {};
 var path = require('path');
 var express = require('express');
 var app = express();
+var session = require("express-session");
 var cookieparser = require('cookie-parser')();
 var ejs = require('ejs');
 
 function Story(options) {
-  this.title = options.Title;
-  this.author = options.Author;
-  this.public = (options.Public === "no") ? false : true;
-  this.fragments = options.Fragments;
-  this.file = options.file;
+  this.Title = options.Title;
+  this.Author = options.Author;
+  this.Public = (options.Public === "no") ? false : true;
+  this.Fragments = options.Fragments;
+  this.File = options.File;
 }
 
 app.set('views', './news');
@@ -35,11 +36,16 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(session({
+  secret: 'IVTK4OO8zLMgzia',
+  resave: true,
+  saveUninitialized: true
+}));
+
 app.use('/media', express.static('media'));
 
 app.all('/', function(req, res) {
-  res.writeHead(302, {Location: '/landing.html'});
-  res.end();
+  res.redirect("/landing.html");
 });
 
 app.get('^/landing.html$', function(req, res) {
@@ -68,13 +74,62 @@ app.all('^/logout/?$', function(req, res) {
   logout(res);
 });
 
+app.get("^/buy/*.story$", function(req, res) {
+  var fileName = path.basename(req.path);
+  getStory(fileName, function(story) {
+    app.render("buy.html",
+      {
+        story: story,
+        username: req.usernameDict.username,
+        role: req.usernameDict.role
+      }, function(err, html) {
+        res.send(html);
+    });
+  });
+});
+
+app.get("^/payment/*$", function(req, res) {
+  req.session.email = req.query.email;
+  req.session.address = req.query.address;
+  req.session.story = new Story(JSON.parse(req.query.story));
+  app.render("payment.html",
+    {
+      username: req.usernameDict.username,
+      role: req.usernameDict.role,
+      session: req.session
+    }, function(err, html){
+      res.send(html);
+  });
+});
+
+app.get("^/confirmation/*$", function(req, res) {
+  req.session.creditCard = req.query.creditCard;
+  req.session.billingAddress = req.query.billingAddress;
+  app.render("confirmation.html", {
+    username: req.usernameDict.username,
+    role: req.usernameDict.role,
+    session: req.session
+  }, function(err, html) {
+    if (err) {
+      console.log(err);
+    }
+    res.send(html);
+  });
+});
+
+app.get("^/confirmed/?$", function(req, res) {
+  var story = req.session.story;
+  req.session = null;
+  res.redirect("/" + story.File);
+});
+
 app.all('^*.story$', function(req, res) {
   var fullpath = path.join(__dirname, path.basename(req.path));
   fs.readFile(fullpath, 'utf-8', function(err, data) {
     var obj = JSON.parse(data);
-    obj.file = path.basename(fullpath);
+    obj.File = path.basename(fullpath);
     var story = new Story(obj);
-    var fragmentPaths = story.fragments.map(function(fragment) {
+    var fragmentPaths = story.Fragments.map(function(fragment) {
       return path.join("news", fragment);
     });
     var HTMLFragments = [];
@@ -188,6 +243,14 @@ function editStory(res, requrl, templateName){
   });
 }
 
+function getStory(story, callback) {
+  fs.readFile(story, 'utf-8', function(err, data){
+    storyObj = new Story(JSON.parse(data));
+    storyObj.File = story;
+    callback(storyObj);
+  });
+}
+
 function getStories(callback) {
   var result = [];
   var ROOT = __dirname;
@@ -202,7 +265,7 @@ function getStories(callback) {
     function inner(error, data) {
       var fileName = path.basename(paths.pop());
       var obj = JSON.parse(data);
-      obj.file = fileName;
+      obj.File = fileName;
       result.push(new Story(obj));
       if (paths.length > 0) {
           fs.readFile(paths[paths.length - 1], 'utf-8', inner);
@@ -218,9 +281,9 @@ function renderStory(story, callback) {
   var fullpath = path.join(__dirname, path.basename(story));
   fs.readFile(fullpath, 'utf-8', function(err, data) {
     var obj = JSON.parse(obj);
-    obj.file = path.basename(fullpath);
+    obj.File = path.basename(fullpath);
     var story = new Story(obj);
-    var fragmentPaths = story.fragments.map(function(fragment) {
+    var fragmentPaths = story.Fragments.map(function(fragment) {
       return path.join("news", fragment);
     });
     var HTMLFragments = [];
